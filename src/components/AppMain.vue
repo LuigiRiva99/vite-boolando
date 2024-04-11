2b574c34bc883632944b1d07d0fc2b8cb7be63d8
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-04-11 17:00:23 +0200

    REFACTOR: db.json file

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,116 +1,13 @@
 <script>
 import AppCard from './AppCard.vue'
-
+import cardProducts from '../assets/db.json'
 export default {
     components : {
         AppCard
     },
     data() {
         return {
-            "products": [
-                {
-                    "id": 1,
-                    "frontImage": "1.webp",
-                    "backImage": "1b.webp",
-                    "brand": "Levi's",
-                    "name": "Relaxed Fit",
-                    "price": 29.99,
-                    "isInFavorites": true,
-                    "badges": [
-                        {
-                            "type": "tag",
-                            "value": "Sostenibilità"
-                        },
-                        {
-                            "type": "discount",
-                            "value": "-50%"
-                        }
-                    ]
-                },
-                {
-                    "id": 2,
-                    "frontImage": "2.webp",
-                    "backImage": "2b.webp",
-                    "brand": "Guess",
-                    "name": "Roses Tee",
-                    "price": 20.99,
-                    "isInFavorites": true,
-                    "badges": [
-                        {
-                            "type": "discount",
-                            "value": "-30%"
-                        }
-                    ]
-                },
-                {
-                    "id": 3,
-                    "frontImage": "3.webp",
-                    "backImage": "3b.webp",
-                    "brand": "Come Zucchero Filato",
-                    "name": "Voglia di colori pastello",
-                    "price": 129.99,
-                    "isInFavorites": false,
-                    "badges": [
-                        {
-                            "type": "discount",
-                            "value": "-30%"
-                        }
-                    ]
-                },
-                {
-                    "id": 4,
-                    "frontImage": "4.webp",
-                    "backImage": "4b.webp",
-                    "brand": "Levi's",
-                    "name": "Tee Unisex",
-                    "price": 14.99,
-                    "isInFavorites": false,
-                    "badges": [
-                        {
-                            "type": "tag",
-                            "value": "Sostenibilità"
-                        },
-                        {
-                            "type": "discount",
-                            "value": "-50%"
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "frontImage": "5.webp",
-                    "backImage": "5b.webp",
-                    "brand": "Maya Deluxe",
-                    "name": "Stripe Bodice",
-                    "price": 99.99,
-                    "isInFavorites": true,
-                    "badges": [
-                        {
-                            "type": "tag",
-                            "value": "Sostenibilità"
-                        },
-                        {
-                            "type": "discount",
-                            "value": "-50%"
-                        }
-                    ]
-                },
-                {
-                    "id": 6,
-                    "frontImage": "6.webp",
-                    "backImage": "6b.webp",
-                    "brand": "Esprit",
-                    "name": "Maglione - Black",
-                    "price": 29.99,
-                    "isInFavorites": true,
-                    "badges": [
-                        {
-                            "type": "tag",
-                            "value": "Sostenibilità"
-                        }
-                    ]
-                }
-            ]
+            cardProducts: cardProducts.products,
         }
     }
 }
@@ -121,7 +18,7 @@ export default {
         <section class="product-page">
             <div class="container">
                 <div class="row">
-                    <AppCard  v-for="(product,i) in products" :key="i" :element="product"/>
+                    <AppCard  v-for="(product,i) in cardProducts" :key="i" :element="product"/>
                     <!-- <div class="col-4" v-for="product in products" :key="i">
                         <div class="product-item">
                             <div class="product-photo">
